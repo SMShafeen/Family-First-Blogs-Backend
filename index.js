2c53104714af02651adedc4b730191cdb13ecb89
SMShafeen <shafeenpc123@gmail.com>
2025-11-15 13:37:31 +0530

    Update backend

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import { env } from "process";
 const app = express();
 dotenv.config();
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use("/webhooks", webhookRouter);
 app.use(express.json());
@@ -76,13 +76,13 @@ app.use((error, req, res, next) => {
     });
 });
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+// if(process.env.NODE_ENV === 'production'){
+//   app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-  app.get(/(.*)/, (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get(/(.*)/, (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 const port = process.env.PORT;
 app.listen(port, ()=>{
